6921f60be8d985614600ddf13fd4d6361e9c03dd
Marcos <33052423+markettes@users.noreply.github.com>
2019-04-09 00:31:18 +0200

    3rd sol

--- factorialize.js
+++ factorialize.js
@@ -25,3 +25,17 @@ function factorialize(num){
 console.log(factorialize(5));
 console.log(factorialize(10));
 console.log(factorialize(15));
+
+
+//3rd 
+
+function factorialize(num){
+  for(var i = 1; num >= 1; num--){
+    i *= num;
+  }
+  return i;
+}
+
+console.log(factorialize(5));
+console.log(factorialize(10));
+console.log(factorialize(15));
