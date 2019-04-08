50dc81f51e4a38d776ebc26b0744272a31be4f2c
Marcos <33052423+markettes@users.noreply.github.com>
2019-04-09 00:22:29 +0200

    2nd sol

--- factorialize.js
+++ factorialize.js
@@ -13,3 +13,15 @@ function factorialize(num){
 console.log(factorialize(5));
 console.log(factorialize(10));
 console.log(factorialize(15));
+
+
+//2nd 
+
+function factorialize(num){
+  if(num === 0) return 1;
+  return num * factorialize(num - 1);
+}
+
+console.log(factorialize(5));
+console.log(factorialize(10));
+console.log(factorialize(15));
