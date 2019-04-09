9dbdd4039a05441b7cf5e4823c1e27bdf24ecd52
Marcos <33052423+markettes@users.noreply.github.com>
2019-04-09 16:55:19 +0200

    1st sol

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+//Check for Palindromes
+
+//1st Solution
+
+function palindrome(str){
+  let front = 0;
+  let back  = str.length - 1;
+  
+  while(front < back){
+    if(str[front] != str[back]) return false
+    front++; back--;
+  }
+  
+  return true;
+  
+}
+
+console.log(palindrome("casa"));
+console.log(palindrome("mom"));
+console.log(palindrome("aaaaapppaaaaa"));
